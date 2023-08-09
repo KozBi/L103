c84589065f19e710ef9e907e9e79f89dd5bc8367
KozBi <kozlo7830@gmail.com>
2023-08-09 20:04:55 +0200

    2nd commit

--- buble_sort.py
+++ buble_sort.py
@@ -1,5 +1,8 @@
 
 def buble_sort(variable):
+    try:
+        variable=list(variable)
+    except: "Cannot be converted into list"
     temp=variable
     try:
         if len(variable)==0:
@@ -24,11 +27,20 @@ def buble_sort(variable):
                 if flag==False:
                     break
             return(temp)
-    except:print("Only list")
+    except:print("Only list allowed")
     
 
-argument=[1,2,5,3,1,7,9,1,12,83,1,5,3,2]
+argument1=None
+argument2=[1,2,5,3,1,7,9,1,12,83,1,5,3,2]
+argument3=(1,1,1,1,1,1,1,2,1,1,1)
+argument4=(2,2,2,2)
+argument5=(1,2)
+argument6=(2,1)
 
-argument=[6,1,2,3,4,5]
-print(buble_sort(argument))
+print(buble_sort(argument1))
+print(buble_sort(argument2))
+print(buble_sort(argument3))
+print(buble_sort(argument4))
+print(buble_sort(argument5))
+print(buble_sort(argument6))
                     
